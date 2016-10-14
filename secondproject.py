2384a22b987c0fc0dbb68ebff118e0cc3ba07d14
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-10-13 22:47:50 -0400

    Thursday night. Code believed to be functionally complete.

--- secondproject.py
+++ secondproject.py
@@ -2,56 +2,72 @@
 
 import random
 
-#COUNT INVALID GUESSES?
-count = 0
-answer = 3#random.randrange(1,101)
+answer = random.randrange(1,101)
 lieUsed = False
+lieInfo = ""
 
 def validate(guess):
 	"""Takes the user's guess and checks if it is a valid,
-	natural number."""
+	natural number between 1 and 100."""
 	try:
 		guess = int(guess)
+		assert guess >= 1 and guess <= 100 
 	except:
 		print(guess, "is not a valid guess -", end=" ")
 		return False
 	return guess
 
-def compareAnswer(guess):
+def compareAnswer(guess,truth):
 	"""Gives the player information on whether
 	their guess was higher or lower than the
 	randomly generated correct answer"""
+	high = "is too high -"
+	low = "is too low -"
+	position = "" #only really used for a lie
+	if not truth:
+		low, high = high, low
 	if guess > answer:
-		print(guess, "is too high -", end=" ")
+		print(guess, high, end=" ")
+		position = "low"
 	elif guess < answer:
-		print(guess, "is too low -", end=" ")
+		print(guess, low, end=" ")
+		position = "high"
+	return (guess, position)
 
-def ulamsLie(guess):
-	"""Gives the player incorrect information on 
-	whether their guess was higher or lower than the
-	randomly generated correct answer"""
-	if guess < answer:
-		print(guess, "is too high -", end=" ")
-	elif guess > answer:
-		print(guess, "is too low -", end=" ")
-	
 
+#Initial prompt, and checks for validity and relative position to correct answer.	
 print("I'm thinking of a number between 1 and 100.")
 guess = input("Try to guess my number: ")
-
-while True:
-	validate(guess)
-	compareAnswer(guess)
+guess = validate(guess)
+count = 0
+if guess:
+	compareAnswer(guess,True)
 	count += 1
+
+#All subsequent guesses. Loops until answer is true. During the loop,
+#if the guess is incorrect, validity is checked, a decision is made
+#regarding whether or not a one time lie is to occur that round,
+#and the guess is checked for relative position to answer.
+#Truthfullness will depend on the previous check.
+while True:
 	if guess == answer:
 		print(guess, "is correct! You guessed my number in",\
-		      count, "guesses.")
+		      count, "guesses.",lieInfo)
 		break
 
 	else:
 		guess = validate(input("please guess again: "))
-		if not lieUsed:
+		if (guess and not lieUsed):
+			count += 1
 			lieChance = random.randrange(1,5)
 			if lieChance == 3:
-				ulamsLie(guess)
-		compareAnswer(guess)
+				lie = compareAnswer(guess,False)
+				lieInfo = "I lied about " + str(lie[0]) + " being too " + lie[1]
+				lieUsed = True
+			else:
+				compareAnswer(guess,True)
+		elif guess:
+			count += 1
+			compareAnswer(guess,True)
+		else:
+			pass
