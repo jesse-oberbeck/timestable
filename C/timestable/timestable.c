bf0dd9a3508b19ccc18292ecd2bb2550874759aa
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-10-21 13:16:42 -0400

    Possibly done with C project 1. Possible comment changes or refinements to come.

--- C/timestable/timestable.c
+++ C/timestable/timestable.c
@@ -4,27 +4,55 @@
 
 int main(int argc, char *argv[])
 {
-	int vertical_array[11];
-	int horizontal_array[11];
-	long int number = 10;
-	if(argc > 1){
-		number = strtol(argv[1], NULL, 10);
-	}
-	//printf("%ld\n",number);
-	for(int x = 0; x < number; x++){
-		vertical_array[x] = x + 1;
-		horizontal_array[x] = x + 1;
-		//printf("%d   ",x);
-	}
-	
-	int vertical_index = 0;
-	int horizontal_index = 0;
-	for(int vert = 0; vert < number; vert++){
-		vertical_index = vert;
-		for(int horiz = 0; horiz < number; horiz++){
-			horizontal_index = horiz;
-			printf("%d   ", vertical_array[vertical_index] * horizontal_array[horizontal_index]);
-		}
-	puts("");
-	}
+    int vertical_array[100], horizontal_array[100];
+    long int upper_bound = 10, lower_bound = 1;
+    int width = 4;
+    char * side_border = "\u2503";
+    char * top_border = "\u2501";
+    //Checks for command line args.
+    if(argc == 2){
+        upper_bound = strtol(argv[1], NULL, 10);
+    }else if(argc == 3){
+        upper_bound = strtol(argv[2], NULL, 10);
+        lower_bound = strtol(argv[1], NULL, 10);
+    }
+    if(upper_bound > 39){
+        width = 5; //Adjustment for longer numbers.
+    }
+    //Minor input validation.
+    if((upper_bound < 0)||(lower_bound < 0)){
+        puts("Invalid input. Positive integers only.\nProgram will now close.");
+        return(1);
+    }else if((upper_bound > 100)||(lower_bound > 100)){
+        puts("Integers must be less than 101.");
+        return(1);
+    }
+    //Builds arrays within the default or user supplied limits.
+    for(int x = 0; x <= upper_bound; x++){
+        vertical_array[x] = x;
+        horizontal_array[x] = x;
+    }
+    //Loop and inner loop multiply one factor by another based on index.
+    for(int vert = lower_bound - 1; vert <= upper_bound; vert++){
+        if(vert == lower_bound - 1){
+            printf(" %*s", width,"   * ");
+            int index = vert+1;
+            while(index <= upper_bound){ //loop prints factors and top border.
+                printf(" %*d", width,horizontal_array[index]);
+                index++;
+            }
+            int number_of_bars = ((upper_bound - lower_bound) * (width + 1) + width + 1);
+            puts("");
+            printf("%*s%s", width + 1, " ", "\u254B"); //Sets up the corner of the border.
+            for(int count = 0; count < number_of_bars; count++){
+                printf("%s", top_border);
+            }
+        }else{
+            printf(" %*d%s", width, vertical_array[vert],side_border); //prints side factors.
+            for(int horiz = lower_bound; horiz <= upper_bound; horiz++){
+                printf(" %*d", width, vertical_array[vert] * horizontal_array[horiz]);
+                }        
+    }
+    puts("");
+    }
 }
--- C/timestable/timestable2.c
+++ C/timestable/timestable2.c
@@ -4,39 +4,39 @@
 
 int main(int argc, char *argv[])
 {
-	int vertical_array[101];
-	int horizontal_array[101];
+	int vertical_array[100];
+	int horizontal_array[100];
 	long int upper_bound = 10;
 	long int lower_bound = 1;
+	//Checks for command line args.
 	if(argc == 2){
 		upper_bound = strtol(argv[1], NULL, 10);
 	}else if(argc == 3){
 		upper_bound = strtol(argv[2], NULL, 10);
 		lower_bound = strtol(argv[1], NULL, 10);
 	}
-	//printf("%ld\n",upper_bound);
+	//Builds arrays within the default or user supplied limits.
 	for(int x = 0; x <= upper_bound; x++){
 		vertical_array[x] = x;
 		horizontal_array[x] = x;
-		//printf("%d   ",x);
 	}
-	
+	//Loop and inner loop multiply one factor by another based on index.
 	int vertical_index = 0;
 	int horizontal_index = 0;
 	for(int vert = lower_bound - 1; vert <= upper_bound; vert++){
 		if(vert == lower_bound - 1){
-			printf("%4s","   *");
+			printf(" %*s",12,"   *");
 			int index = vert+1;
 			while(index <= upper_bound){ //loop prints top border.
-				printf("%4d",horizontal_array[index]);
+				printf(" %*d",12,horizontal_array[index]);
 				index++;
 			}
 		}else{
-			printf("%4d", vertical_array[vert]); //prints side border.
+			printf(" %*d",12, vertical_array[vert]); //prints side border.
 			vertical_index = vert;
 			for(int horiz = lower_bound; horiz <= upper_bound; horiz++){
 				horizontal_index = horiz;
-				printf("%4d", vertical_array[vertical_index] * horizontal_array[horizontal_index]);
+				printf(" %*d",12, vertical_array[vertical_index] * horizontal_array[horizontal_index]);
 				}
 
 		
