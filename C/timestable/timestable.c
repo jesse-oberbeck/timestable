5e3ba0190ac238d66d3479dc125f9d3cf8dcc94f
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-10-21 14:59:50 -0400

    Small changes to variables and comments, adjust size for smaller upper bounds, and more flags added to Makefile.

--- C/timestable/timestable.c
+++ C/timestable/timestable.c
@@ -4,11 +4,11 @@
 
 int main(int argc, char *argv[])
 {
+    //Sets up arrays and default values.
     int vertical_array[100], horizontal_array[100];
     long int upper_bound = 10, lower_bound = 1;
     int width = 4;
-    char * side_border = "\u2503";
-    char * top_border = "\u2501";
+
     //Checks for command line args.
     if(argc == 2){
         upper_bound = strtol(argv[1], NULL, 10);
@@ -16,43 +16,61 @@ int main(int argc, char *argv[])
         upper_bound = strtol(argv[2], NULL, 10);
         lower_bound = strtol(argv[1], NULL, 10);
     }
-    if(upper_bound > 39){
-        width = 5; //Adjustment for longer numbers.
+
+    //Adjustment for longer or shorter numbers.
+    if(upper_bound > 32){
+        width = 5;
+    }else if(upper_bound < 20){
+        width = 3;
     }
+
     //Minor input validation.
-    if((upper_bound < 0)||(lower_bound < 0)){
+    if((upper_bound < 0) || (lower_bound < 0)){
         puts("Invalid input. Positive integers only.\nProgram will now close.");
         return(1);
-    }else if((upper_bound > 100)||(lower_bound > 100)){
+    }else if((upper_bound > 100) || (lower_bound > 100)){
         puts("Integers must be less than 101.");
         return(1);
     }
+
     //Builds arrays within the default or user supplied limits.
-    for(int x = 0; x <= upper_bound; x++){
-        vertical_array[x] = x;
-        horizontal_array[x] = x;
+    for(int i = 0; i <= upper_bound; i++){
+        vertical_array[i] = i;
+        horizontal_array[i] = i;
     }
+
     //Loop and inner loop multiply one factor by another based on index.
     for(int vert = lower_bound - 1; vert <= upper_bound; vert++){
         if(vert == lower_bound - 1){
-            printf(" %*s", width,"   * ");
+            printf(" %*s", width,"     ");
             int index = vert+1;
-            while(index <= upper_bound){ //loop prints factors.
-                printf(" %*d", width,horizontal_array[index]);
+
+            //Loop prints top factors.
+            while(index <= upper_bound){
+                printf("%*d", width + 1,horizontal_array[index]);
                 index++;
             }
+
+            //Generates quantity of bars for top border.
             int number_of_bars = ((upper_bound - lower_bound) * (width + 1) + width + 1);
             puts("");
-            printf("%*s%s", width + 1, " ", "\u254B"); //Sets up the corner of the border.
-            for(int count = 0; count < number_of_bars; count++){ //Prints top border.
-                printf("%s", top_border);
+
+            //Sets up the corner of the border.
+            printf("%*s%s", width + 1, " ", "\u250F");
+
+            //Prints top border.
+            for(int count = 0; count < number_of_bars; count++){
+                printf("%s", "\u2501");
             }
         }else{
-            printf(" %*d%s", width, vertical_array[vert],side_border); //prints side factors.
+            //prints side factors and border.
+            printf(" %*d%s", width, vertical_array[vert], "\u2503");
+
+            //Maths happen here.
             for(int horiz = lower_bound; horiz <= upper_bound; horiz++){
                 printf(" %*d", width, vertical_array[vert] * horizontal_array[horiz]);
-                }        
-    }
+            }        
+        }
     puts("");
     }
 }
